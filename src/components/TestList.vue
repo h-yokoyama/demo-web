b041796dcf2869ff731e0fa32c5ae1916af8840c
h-yokoyama <milkmccya@gmail.com>
2020-10-10 17:30:06 +0900

    first commit

---
+++ src/components/TestList.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-button size="mini" type="primary" @click="showModal">編集</el-button>
+</template>
+
+<script>
+export default {
+  methods: {
+    showModal() {
+      this.$emit("onModal");
+    }
+  }
+};
+</script>
---
+++ src/views/Currency.vue
@@ -0,0 +1,151 @@
+<template>
+  <el-row>
+    <el-col :span="24">
+      <el-card class="box-card box-card-wrapper">
+        <div slot="header">
+          <span>新規通貨追加</span>
+        </div>
+        <el-row class="row-wrapper">
+          <el-col :span="12">
+            <span>新規通貨名称</span>
+          </el-col>
+          <el-col :span="12">
+            <el-input
+              v-model="request.name"
+              placeholder="New Name..."
+              clearable
+            >
+            </el-input>
+          </el-col>
+        </el-row>
+        <el-row class="row-wrapper">
+          <el-col :span="12">
+            <span>新規通貨シンボル</span>
+          </el-col>
+          <el-col :span="12">
+            <el-input
+              v-model="request.symbol"
+              placeholder="New Symbol..."
+              clearable
+            >
+            </el-input>
+          </el-col>
+        </el-row>
+        <el-row class="row-wrapper">
+          <el-col :span="24">
+            <el-button type="success" @click="addCurrency">追加</el-button>
+          </el-col>
+        </el-row>
+      </el-card>
+    </el-col>
+    <el-col :span="24">
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>仮想通貨一覧</span>
+        </div>
+        <el-table :data="currencies" style="width: 100%">
+          <el-table-column prop="id" label="通貨ID" width="300" />
+          <el-table-column prop="name" label="通貨名" width="300" />
+          <el-table-column prop="symbol" label="通貨単位" width="300" />
+          <el-table-column prop="amount" label="数量" width="300" />
+          <el-table-column
+            prop="operation"
+            label="Ops"
+            width="200"
+            align="left"
+          >
+            <template slot-scope="scope">
+              <test-list v-on:onModal="onModal(true, scope.row)" />
+              <test-modal
+                v-bind:dialogVisible="dialogVisible"
+                v-bind:val="selectedCurrency"
+                v-on:onModal="onModal(false, selectedCurrency)"
+                v-on:updateCurrency="updateCurrency(selectedCurrency)"
+              />
+              <el-button
+                size="mini"
+                type="danger"
+                @click="deleteCurrency(scope.row.id)"
+                >×</el-button
+              >
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-card>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import axios from "axios";
+import TestList from "@/components/TestList.vue";
+import TestModal from "@/components/TestModal.vue";
+
+export default {
+  name: "Currency",
+  components: {
+    TestList,
+    TestModal
+  },
+
+  data() {
+    return {
+      request: {
+        name: undefined,
+        symbol: undefined
+      },
+      currencies: [],
+      dialogVisible: false,
+      selectedCurrency: undefined
+    };
+  },
+  created: async function() {
+    await this.refresh();
+  },
+  methods: {
+    refresh: async function() {
+      const ret = await axios.get(process.env.VUE_APP_API_URL);
+      this.currencies = ret.data.currencies;
+      this.request.name = undefined;
+      this.request.symbol = undefined;
+    },
+    addCurrency: async function() {
+      await axios.post(process.env.VUE_APP_API_URL, this.request);
+      await this.refresh();
+      this.$message({
+        showClose: true,
+        message: "Add Currency Success!",
+        type: "success"
+      });
+    },
+    deleteCurrency: async function(id) {
+      await axios.delete(process.env.VUE_APP_API_URL + id);
+      await this.refresh();
+      this.$message({
+        showClose: true,
+        message: "Delete Currency Success!",
+        type: "success"
+      });
+    },
+    updateCurrency: async function(selectedItem) {
+      await console.log(selectedItem);
+      await axios.put(process.env.VUE_APP_API_URL, selectedItem);
+      await this.refresh();
+      this.$message({
+        showClose: true,
+        message: "Update Currency Success!",
+        type: "success"
+      });
+      this.dialogVisible = false;
+    },
+    onModal: function(dialogVisible, selectedItem) {
+      this.selectedCurrency = selectedItem;
+      this.dialogVisible = dialogVisible;
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import "../styles/base";
+</style>
